8713268954b62d39c357fa288810253cbe5ab2ab
Valentyn Shvets <valentynshvets@icloud.com>
2019-09-30 14:48:45 +0300

    Deleted Secret key

--- Coax/settings.py
+++ Coax/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secret import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'egbjtthi%$##ounf)r=_feg5%_=27xep%9-f1s1p22-yk(kjy^'
-
+# SECURITY WARNING: keep the secret.py key used in production secret.py!
+SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
